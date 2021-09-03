6799a969235a91dac01c80ccd38c36d9f1690fe2
Rico Apon <rico.apon@gmail.com>
2021-09-03 15:36:54 +0200

    Install AngularFire package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC9yU9uVV4t8X-0x7j_XWh73J3p4zKcQyI',
+    authDomain: 'ticket-system-f5428.firebaseapp.com',
+    projectId: 'ticket-system-f5428',
+    storageBucket: 'ticket-system-f5428.appspot.com',
+    messagingSenderId: '312274026604',
+    appId: '1:312274026604:web:79105699b11b7a71405849'
+  }
 };
 
 /*
