b22b537ae52c4cf89386596d19ef04238934f483
Rico Apon <rico.apon@gmail.com>
2021-09-03 16:15:14 +0200

    Add Firebase connection in production environment data as well

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC9yU9uVV4t8X-0x7j_XWh73J3p4zKcQyI',
+    authDomain: 'ticket-system-f5428.firebaseapp.com',
+    projectId: 'ticket-system-f5428',
+    storageBucket: 'ticket-system-f5428.appspot.com',
+    messagingSenderId: '312274026604',
+    appId: '1:312274026604:web:79105699b11b7a71405849'
+  }
 };
